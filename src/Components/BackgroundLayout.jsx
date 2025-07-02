36ce59b08b82c208a7e4e7c43e6984db67770e91
Manikandan Subramanian <geethamanimurugan2005@gmail.com>
2025-07-02 22:20:26 +0530

    Update BackgroundLayout.jsx
    snow and fog are capitalized then match the exact name in the images

--- src/Components/BackgroundLayout.jsx
+++ src/Components/BackgroundLayout.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { useStateContext } from '../Context'
 import Clear from '../assets/images/Clear.jpg'
-import Fog from '../assets/images/Fog.png'
+import Fog from '../assets/images/fog.png'
 import Cloudy from '../assets/images/Cloudy.jpg'
 import Rainy from '../assets/images/Rainy.jpg'
-import Snow from '../assets/images/Snow.jpg'
+import Snow from '../assets/images/snow.jpg'
 import Stromy from '../assets/images/Stormy.jpg'
 import Sunny from '../assets/images/Sunny.jpg'
 
